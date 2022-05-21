fc4a01dfdaed4333706aea9e0fcec499a3c5c421
sufuwang <1137120948@qq.com>
2022-05-21 22:33:08 +0800

    feat: init

--- configuration/dev/install.sh
+++
@@ -1,9 +0,0 @@
-docker image rm nginx_image -f
-docker container rm nginx_container -f
-docker image rm node_image -f
-docker container rm node_container -f
-
-docker build -f ./nginx.dockerfile -t nginx_image .
-docker container run -d -p 9999:80 --name nginx_container nginx_image
-docker build -f ./node.dockerfile -t node_image .
-docker container run -dp 4002:3001 --name node_container node_image
